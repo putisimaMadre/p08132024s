f1dacabdb022aebf20535fdc2cae36f773c4f3d9
GeorgeX <putisima_madre@hotmail.com>
2024-08-13 14:07:29 -0600

    hola mundo

--- src/main/java/com/p08132024/p08132024/P08132024Application.java
+++ src/main/java/com/p08132024/p08132024/P08132024Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class P08132024Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(P08132024Application.class, args);
+		System.out.println("Hola mundo");
+		//SpringApplication.run(P08132024Application.class, args);
 	}
 
 }
--- src/main/java/com/p08132024/p08132024/controllers/UsuarioController.java
+++ src/main/java/com/p08132024/p08132024/controllers/UsuarioController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UsuarioController {
     @GetMapping("/usuario")
-    public String getUsuario(){
-        return "Hola mundo";
+    public void getUsuario(){
+        System.out.println("Hola mundo");
     }
 }
